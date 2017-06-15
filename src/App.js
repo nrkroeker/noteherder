23d5a4a68a98ca1fe7f4e148caedc92acb664641
nrkroeker <nathaliekroeker@gmail.com>
2017-06-15 14:23:57 -0400

    Fix form functionality

--- src/App.js
+++ src/App.js
@@ -9,24 +9,26 @@ class App extends Component {
 
     this.state = {
       notes: {
-        'note-1': {
-          id: 'note-1',
-          title: 'Thoughts on React',
-          body: 'React is pretty nifty. Declarative FTW! 🎸',
-        },
-        'note-2': {
-          id: 'note-2',
-          title: 'State and props?',
-          body: 'Wat',
-        },
+
       },
     }
+  }
+
+  saveNote = (note) => {
+    if (!note.id) {
+      note.id = `note-${Date.now()}`
+    }
+
+    const notes = {...this.state.notes}
+    notes[note.id] = note
+    this.setState({ notes})
+
   }
 
   render() {
     return (
       <div className="App">
-        <Main notes={this.state.notes} />
+        <Main notes={this.state.notes} saveNote={this.saveNote} />
       </div>
     );
   }
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,15 +3,47 @@ import React, { Component } from 'react'
 import './NoteForm.css'
 
 class NoteForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      note: this.blankNote(),
+    }
+  }
+
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
+
+  handleChanges = (ev) => {
+    const note = {...this.state.note}
+    note[ev.target.name] = ev.target.value
+    this.setState(
+      { note },
+      () =>  this.props.saveNote(this.state.note)
+    )
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.setState({ note: this.blankNote() })
+  }
+
   render() {
     return (
       <div className="NoteForm">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>
             <input
               type="text"
               name="title"
               placeholder="Title your note"
+              onChange={this.handleChanges}
+              value={this.state.note.title}
             />
           </p>
           <p>
@@ -20,8 +52,11 @@ class NoteForm extends Component {
               cols="30"
               rows="10"
               placeholder="Just start typing..."
+              onChange={this.handleChanges}
+              value={this.state.note.body}
           ></textarea>
           </p>
+          <button type="submit">Save and Reset</button>
         </form>
       </div>
     )
